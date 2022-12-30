b7ba8a11c6509d9824ca1c1699c11839329a83f8
ruben godde <ruben.godde@student.uhasselt.be>
2022-12-30 09:19:09 +0100

    updated JPanels in wedstrijdcontroller to be more user friendly

--- src/main/java/be/kuleuven/vrolijkezweters/controller/BeheerWedstrijdenController.java
+++ src/main/java/be/kuleuven/vrolijkezweters/controller/BeheerWedstrijdenController.java
@@ -8,13 +8,18 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import org.jdesktop.swingx.JXDatePicker;
 
 import javax.swing.*;
+import java.awt.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.List;
+
+import static com.sun.javafx.application.PlatformImpl.exit;
 
 public class BeheerWedstrijdenController {
     private List<Wedstrijd> wedstrijdList;
@@ -85,7 +90,55 @@ public class BeheerWedstrijdenController {
             wedstrijd.setCategorieId(categorie);
         }
     }
+    private void addNewRow(){
+        JTextField naam = new JTextField();
+        JXDatePicker picker = new JXDatePicker();
+        JTextField plaats = new JTextField();
+        JTextField inschrijvingsGeld = new JTextField();
 
+        picker.setDate(Calendar.getInstance().getTime());
+        picker.setFormats(new SimpleDateFormat("dd/MM/yyyy"));
+
+        List<Categorie> categorieList = ConnectionManager.handle.createQuery("SELECT * FROM Categorie")
+                .mapToBean(Categorie.class)
+                .list();
+        String[] choices = new String[categorieList.size()];
+        for(int i = 0 ; i < categorieList.size(); i++){
+            choices[i] = categorieList.get(i).toString().replace("Categorie{categorie'","").replace("}","");
+        }
+        final JComboBox<String> category = new JComboBox<String>(choices);
+
+        Object[] message = { "naam: ", naam, "datum: ", picker, "plaats: ", plaats, "inschrijvingsgeld: ", inschrijvingsGeld, "categorie: ", category};
+        String[] buttons = { "Save", "Cancel" };
+
+        int option = JOptionPane.showOptionDialog(null, message, "Add Wedstrijd", JOptionPane.OK_CANCEL_OPTION, 0, null, buttons, buttons[0]);
+
+        //TODO check juiste ingave en formaten
+        if(option == JOptionPane.OK_OPTION){
+            if("check if correct".equals("check if correct")){
+                Date date = picker.getDate();
+                DateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+                String dateFormatted = format.format(date);
+
+                tblConfigs.getItems().add(FXCollections.observableArrayList(naam.getText(), dateFormatted, plaats.getText(), inschrijvingsGeld.getText(), category.getSelectedItem()));
+                int cIndex = category.getSelectedIndex() + 1;
+                ConnectionManager.handle.execute("INSERT INTO Wedstrijd (naam, datum, plaats, inschrijvingsgeld, categorieid) values ('" +
+                        naam.getText() +"', '"+
+                        dateFormatted+"', '"+
+                        plaats.getText()+"', '"+
+                        inschrijvingsGeld.getText()+"', '"+
+                        cIndex+"')");
+            }
+        }
+        else{
+            exit();
+            System.out.println("row add canceled");
+        }
+
+    }
+
+    /////////////////////////
+    /*
     private void addNewRow() {
         JTextField naam = new JTextField(5);
         JXDatePicker picker = new JXDatePicker();
@@ -106,20 +159,23 @@ public class BeheerWedstrijdenController {
         final JComboBox<String> category = new JComboBox<String>(choices);
 
         JPanel myPanel = new JPanel();
-        myPanel.add(new JLabel("naam:"));
+        myPanel.setLayout(new BoxLayout(myPanel, BoxLayout.Y_AXIS));
+        myPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        myPanel.add(new JLabel("naam:",SwingConstants.LEFT));
         myPanel.add(naam);
-        myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+        //myPanel.add(Box.createHorizontalStrut(15)); // a spacer
         myPanel.add(new JLabel("datum:"));
         myPanel.add(picker);
-        myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+        //myPanel.add(Box.createHorizontalStrut(15)); // a spacer
         myPanel.add(new JLabel("plaats:"));
         myPanel.add(plaats);
-        myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+        //myPanel.add(Box.createHorizontalStrut(15)); // a spacer
         myPanel.add(new JLabel("inschrijvingsgeld:"));
         myPanel.add(inschrijvingsGeld);
-        myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+        //myPanel.add(Box.createHorizontalStrut(15)); // a spacer
         myPanel.add(new JLabel("categorie:"));
         myPanel.add(category);
+
 
         int result = JOptionPane.showConfirmDialog(null, myPanel,
                 "Please Enter Values", JOptionPane.OK_CANCEL_OPTION);
@@ -137,6 +193,8 @@ public class BeheerWedstrijdenController {
                 inschrijvingsGeld.getText()+"', '"+
                 cIndex+"')");
     }
+    */
+    ///////////////////////////////////
 
     private void deleteCurrentRow() {
         List<Object> selectedItems = tblConfigs.getSelectionModel().getSelectedItems();
@@ -155,10 +213,10 @@ public class BeheerWedstrijdenController {
 
 
     private void modifyCurrentRow() {
-        JTextField naam = new JTextField(5);
+        JTextField naam = new JTextField();
         JXDatePicker picker = new JXDatePicker();
-        JTextField plaats = new JTextField(8);
-        JTextField inschrijvingsGeld = new JTextField(5);
+        JTextField plaats = new JTextField();
+        JTextField inschrijvingsGeld = new JTextField();
 
         List<Object> selectedItems = tblConfigs.getSelectionModel().getSelectedItems();
         int index = tblConfigs.getSelectionModel().getFocusedIndex();
@@ -180,43 +238,39 @@ public class BeheerWedstrijdenController {
         for(int i = 0 ; i < categorieList.size(); i++){
             choices[i] = categorieList.get(i).toString().replace("Categorie{categorie'","").replace("}","");
         }
+
         final JComboBox<String> category = new JComboBox<String>(choices);
         category.setSelectedItem(items.get(4));
 
-        JPanel myPanel = new JPanel();
-        myPanel.add(new JLabel("naam:"));
-        myPanel.add(naam);
-        myPanel.add(Box.createHorizontalStrut(15)); // a spacer
-        myPanel.add(new JLabel("datum:"));
-        myPanel.add(picker);
-        myPanel.add(Box.createHorizontalStrut(15)); // a spacer
-        myPanel.add(new JLabel("plaats:"));
-        myPanel.add(plaats);
-        myPanel.add(Box.createHorizontalStrut(15)); // a spacer
-        myPanel.add(new JLabel("inschrijvingsgeld:"));
-        myPanel.add(inschrijvingsGeld);
-        myPanel.add(Box.createHorizontalStrut(15)); // a spacer
-        myPanel.add(new JLabel("categorie:"));
-        myPanel.add(category);
+        Object[] message = { "naam: ", naam, "datum: ", picker, "plaats: ", plaats, "inschrijvingsgeld: ", inschrijvingsGeld, "categorie: ", category};
+        String[] buttons = { "Save", "Cancel" };
 
-        int result = JOptionPane.showConfirmDialog(null, myPanel,
-                "Please Enter Values", JOptionPane.OK_CANCEL_OPTION);
+        int option = JOptionPane.showOptionDialog(null, message, "Add Wedstrijd", JOptionPane.OK_CANCEL_OPTION, 0, null, buttons, buttons[0]);
 
-        Date date = picker.getDate();
-        DateFormat format = new SimpleDateFormat("dd-MM-yyyy");
-        String dateFormatted = format.format(date);
+        //TODO check juiste ingave en formaten
+        if(option == JOptionPane.OK_OPTION){
+            if("check if correct".equals("check if correct")){
+                Date date = picker.getDate();
+                DateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+                String dateFormatted = format.format(date);
 
 
-        tblConfigs.getItems().add(index, FXCollections.observableArrayList(naam.getText(), dateFormatted, plaats.getText(), inschrijvingsGeld.getText(), category.getSelectedItem()));
-        tblConfigs.getItems().remove(index+1);
-        int cIndex = category.getSelectedIndex() + 1;
-        ConnectionManager.handle.execute("UPDATE Wedstrijd SET " +
-                "naam = '" + naam.getText() + "', " +
-                "datum = '" + dateFormatted + "', " +
-                "plaats = '" + plaats.getText() + "', " +
-                "inschrijvingsgeld = '" + inschrijvingsGeld.getText() + "', " +
-                "categorieId = '" + cIndex + "' " +
-                "WHERE naam = '" + items.get(0) + "'");
+                tblConfigs.getItems().add(index, FXCollections.observableArrayList(naam.getText(), dateFormatted, plaats.getText(), inschrijvingsGeld.getText(), category.getSelectedItem()));
+                tblConfigs.getItems().remove(index+1);
+                int cIndex = category.getSelectedIndex() + 1;
+                ConnectionManager.handle.execute("UPDATE Wedstrijd SET " +
+                        "naam = '" + naam.getText() + "', " +
+                        "datum = '" + dateFormatted + "', " +
+                        "plaats = '" + plaats.getText() + "', " +
+                        "inschrijvingsgeld = '" + inschrijvingsGeld.getText() + "', " +
+                        "categorieId = '" + cIndex + "' " +
+                        "WHERE naam = '" + items.get(0) + "'");
+            }
+        }
+        else{
+            exit();
+            System.out.println("row add canceled");
+        }
     }
 
     public void showAlert(String title, String content) {
